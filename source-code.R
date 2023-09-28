9105cb335e48921830cbe792936de254c49fbe66
73azn <amerbinazim@gmail.com>
2023-09-28 13:00:06 +0300

    week2

--- source-code.R
+++ source-code.R
@@ -1,3 +1,4 @@
+library(openxlsx)
 mq = matrix(c(6:11),3,2)
 ## the matrix syntex ("the values in the matrix","row","col")
 mq
@@ -39,11 +40,69 @@ if(x1 >10){
 
 }
 
-mx[1,1] <- 1
-for (i in c(1:12)){
-mx <-  matrix( , 3 , 6)
+mt <- matrix(
+  data = rnorm(25)
+  ,
+  nrow = 5
+  ,
+  ncol = 5
+  
+  , dimnames = list(NULL,c("col1","col2","col3","col4","col5"))
+  
+)
+mt
 
-for (j in c(1:3)){
-  mx[j,i] <- j 
+loop <- vector("list",ncol(mt))
+
+for (i in 1:ncol(mt)) {
+  
+  loop [[i]] <-mean(mt[,i])
+  
+                    
+  
 }
+
+items <- vector('numeric', length = 5)
+sum(items)
+i <- 1
+while (sum(items)!=10) {
+  print(i)
+  i<- i +1
+  print("------------------------------")
+  
+  
+  items<- items+rnorm(length(items))
+  print(items)
+  
 }
+?read.table
+?write.table
+write.table(loop, file = "bruh.csv", append = FALSE, quote = TRUE, sep = ",",
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+
+getwd()
+
+
+?openxlsx
+
+?read.csv()
+testdata <- "testdata.csv"
+dummy_data <- read.csv(testdata)
+
+meow<-table(dummy_data$Age>=20&dummy_data$Age<50&dummy_data$Weight>=93)
+
+rownames(meow) <- c("who under 20 and above 50 and their wight over 93","betwen 20 and 50 and over wight 93")
+meow            
+
+hist(dummy_data$Weight,breaks = 100)          
+
+
+ds1 <- read.csv("DATA_FSB_SET_2.csv", row.names = 1)
+ds1 <- ds1[complete.cases(ds1),]
+ds1
+options(repr.plot.width = 3 , repr.polt.height =3)
+
+ggplot(ds1,aes(Weight,LDL))+
+geom_point(aes(col-Hospital_Visits),size=3)
